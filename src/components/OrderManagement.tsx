430c408ebab95239f1e8a620f7e2c92024b61a01
Shanu <sandeepshanu@gmail.com>
2025-07-28 20:40:27 -0400

    Modified Orders page spacing

--- src/components/OrderManagement.tsx
+++ src/components/OrderManagement.tsx
@@ -450,7 +450,7 @@ const OrderManagement = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
         {filteredOrders.length === 0 ? (
           <div className="col-span-full">
             <Card>
